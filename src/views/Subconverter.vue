b80629ad582ccfdea4d689940e355be2b88c4532
Hikaru <13292778+SawaHikaru@users.noreply.github.com>
2021-07-01 19:01:44 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -555,7 +555,7 @@ export default {
   },
   mounted() {
     this.form.clientType = "clash";
-    this.form.customBackend = "https://subapi.acgnc.cc/sub?";
+    this.form.customBackend = defaultBackend;
     this.form.remoteConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online.ini";
     this.getBackendVersion();
   },
